dba395f5dd8ee311d2e0a086b948eebbad534a3a
Victor Garcés <victorgarcesgg@gmail.com>
2023-01-31 21:54:19 -0400

    Add basic movement

---
+++ Obstacles/Assets/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0f, zValue);
+    }
+}
